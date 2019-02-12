624e28fb2d73e285235711b34dd2092d0c41adfb
Jay Oza <ozajay0207@gmail.com>
2019-02-12 20:04:20 +0530

    question 3 final

--- question3_inter.c
+++ question3_inter.c
@@ -11,199 +11,274 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 #define TOTAL_ELEMENTS 1470
 #define TOTAL_PAGES 37
 #define PAGE_ENTRIES 40
+#define MAIN_MEMORY_SIZE 6
 
 char input_file[] = "input_file.txt";
-int main_arr[TOTAL_ELEMENTS];
-int p1[PAGE_ENTRIES],p2[PAGE_ENTRIES],p3[PAGE_ENTRIES],p4[PAGE_ENTRIES],p5[PAGE_ENTRIES],p6[PAGE_ENTRIES];
-int PMT[TOTAL_PAGES][2];
-int MM[6]={-1,-1,-1,-1,-1,-1};
-int main_memory_counter=0;
 
+int main_arr[TOTAL_ELEMENTS];
+int MM[6][2]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int Page[TOTAL_PAGES][PAGE_ENTRIES];
+
+int main_memory_counter=0;
+int sum_p1=0,sum_p2=0,sum_p3=0;
+int pf_p1=0,pf_p2=0,pf_p3=0;
+int pr_p1=0,pr_p2=0,pr_p3=0;
 
 sem_t *sem1,*sem2,*sem3;
 
+//TO PRINT THE PCB CONTENT
+void print_PCB(int process_state,int process_id,int program_counter,int page_no){
 
+	//CHECK FOR STATE AND PRINT ACCORDINGLY
+	if(process_state==0)
+		printf("State:Running\n");	
+	else if(process_state==1)
+		printf("State:Waiting\n");	
+	else
+		printf("State:Exit\n");	
+	printf("Process Id:%d\n",process_id);
+	printf("Program Counter:%d\n",program_counter);
+	printf("Memory Management Information:%d\n",page_no);
+
+}
+
+//TO PRINT THE MAIN MEMORY CONTENT
+void print_main_memory(){
+
+	
+	printf("Main Memory Content:");
+	printf("\n************************************************************************\n");
+	for(int i=0;i<MAIN_MEMORY_SIZE;i++){		
+		printf("| P(%d):%d |",MM[i][1],MM[i][0]);
+	}
+	printf("\n************************************************************************");
+	printf("\n");
+}
+
+//CHECK FOR PAGE IF NOT FOUND RETURN 0(PAGE FAULT)
+int check_for_page(int page_no){
+
+	for(int i=0;i<MAIN_MEMORY_SIZE;i++){
+		if(MM[i][0]==page_no)
+			return 1;
+	}
+	return 0;
+}
+
+//TO FETCH THE ARRAY VALUES AND DO SUMMATION
 int do_sum(int process_id,int page_no){
+
+	page_no--;
 	int i;
 	int sum=0;
+	int start_index=page_no*40;
+	int end_index=(page_no*40)+PAGE_ENTRIES;
 
-	if(page_no == 25){
+	//CHECKING THE SHARED PAGES FOR CONDITIONS
+
+	//PAGE 25 IS SHARED BETWEEN PROCESS 2 AND 3
+	if(page_no == 24){
 		if(process_id==2){
-			for(i=page_no*40;i<(page_no*40)+20;i++){
-				sum = sum + main_arr[i];
-			}
-			return sum;
+			start_index=page_no*40;
+			end_index=(page_no*40)+20;
 		}
 		if(process_id==3){
-			for(i=(page_no*40)+20;i<(page_no*40)+PAGE_ENTRIES;i++){
-				sum = sum + main_arr[i];
-			}
-			return sum;
+			start_index=(page_no*40)+20;
+			end_index=(page_no*40)+PAGE_ENTRIES;
 		}
 	}
-	if(page_no == 13){
+
+	//PAGE 13 IS SHARED BETWEEN PROCESS 1 AND 3
+	if(page_no == 12){
 		if(process_id==1){
-			for(i=page_no*40;i<(page_no*40)+10;i++){
-				sum = sum + main_arr[i];
-			}
-			return sum;
+			start_index=page_no*40;
+			end_index=(page_no*40)+10;			
 		}
 		if(process_id==2){
-			for(i=(page_no*40)+10;i<(page_no*40)+PAGE_ENTRIES;i++){
-				sum = sum + main_arr[i];
-			}
-			return sum;
+			start_index=(page_no*40)+10;
+			end_index=(page_no*40)+PAGE_ENTRIES;
 		}
 	}
-	for(i=page_no*40;i<(page_no*40)+PAGE_ENTRIES;i++){
+	
+	//CALCULATE THE SUM BASED ON CALCULATED INDICES
+	printf("No of Array Entries Fetched:%d\n",end_index-start_index);
+	for(i=start_index;i<end_index;i++){
 		sum = sum + main_arr[i];
 	
 	}
-	//printf("\n%d %d",page_no,sum);
+	
 	return sum;
 }
 
-void print_main_memory(){
-	printf("Main Memory Content:\n");
-	for(int i=0;i<6;i++){
-		printf("Page : %d\n",MM[i]);
-	}
+//TO PERFORM THE PAGE FAULT , REPLACEMENT AND PAGING STRATEGY
+void do_paging(int *current_page,int *replaced_page,int *pf_p,int *pr_p,int *sum_p,int process_id){
+
+	*replaced_page=-1;
+
+	//RESET MAIN MEMORY COUNTER IF GREATER THAN MEMORY SIZE
+	if(main_memory_counter>=MAIN_MEMORY_SIZE)
+		main_memory_counter=0;
+
+	//CHECK IF THE PAGE IS PRESENT IN MEMORY
+	if(!check_for_page(*current_page)){			
+		
+		//DO NOT COUNT PAGE REPLACEMENT IF MM IS EMPTY (i.e. in the beginning)		
+		if(MM[main_memory_counter][0]!=-1){
+			*replaced_page=MM[main_memory_counter][0];
+			*pr_p=*pr_p+1;
+		}
+
+		*pf_p=*pf_p+1;
+		printf("\nPage Fault:%d\n",*current_page);
+
+		//IF PAGE IS REPLACED THEN PRINT INFORMATION
+		if(*replaced_page!=-1)
+			printf("Replacing Page: %d\n",*replaced_page);		
+
+		//UPDATE THE PAGE IN MAIN MEMORY	
+		MM[main_memory_counter][0]=*current_page;	
+		MM[main_memory_counter][1]=1;
+		main_memory_counter++;	
+		
+		print_main_memory();
+
+	//IF PAGE IS ALREADY IN MAIN MEMORY
+	}else{
+		printf("\nPage Hit:%d\n",*current_page);
+	}		
+
+	//ADD THE SUM OF CURRENT PAGE
+	*sum_p=*sum_p+do_sum(process_id,*current_page);
+
 }
 
-
+//THREAD FUNCTION TO RUN PROCESS 1
 void *p1_fun(void *vargp) 
 { 
+
 	int i,j;
-	int sum=0;
 	int current_page=1;
+	int replaced_page=-1;
 	
-	while(current_page<=11){
-		if(main_memory_counter>=6)
-			main_memory_counter=0;
+	//PERFORM PAGING FOR NON-SHARED PAGES IN FCFS ORDER
+	while(current_page<=11){	
 		sem_wait(sem1);
-		//for(i=0;i<2 && current_page<=11;i++){
-			//printf("\nPage Fault:%d",current_page);
-			MM[main_memory_counter++]=current_page;			
-			print_main_memory();
-			sum=sum+do_sum(1,current_page);
-			current_page++;
-		//}
+		do_paging(&current_page,&replaced_page,&pf_p1,&pr_p1,&sum_p1,1);
+		current_page++;
 		sem_post(sem2);
-		sleep(2);
 	}
 	
+	//MANAGING THE SHARED PAGES EXPLICITLY
 	sem_wait(sem1);
-	MM[main_memory_counter++]=13;			
-	print_main_memory();
-	sum=sum+do_sum(1,13);
+	current_page=13;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p1,&pr_p1,&sum_p1,1);
 	sem_post(sem2);
-	sleep(2);
 
-	sem_wait(sem1);
-	MM[main_memory_counter++]=12;			
-	print_main_memory();
-	sum=sum+do_sum(1,12);
+	sem_wait(sem1);	
+	current_page=12;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p1,&pr_p1,&sum_p1,1);
 	sem_post(sem2);
-	sleep(2);
+
+	//PRINT THE FINAL SUM CALCULATED
+	printf("Sum by Process 1 :%d\n",sum_p1);
+
+
+	//WAIT FOR BOTH THE PROCESS 2 AND 3 TO RETURN THE FINAL CONTENTS
+	sem_wait(sem1);
+	printf("\n************************************************************************\n");
+	printf("Summing up partial sum of the processes...\n");
+	sum_p1=sum_p1+sum_p2+sum_p3;
+	printf("Final Sum : %d\n",sum_p1);	
+
+	printf("\nPage Faults:\n");
+	printf("Page Fault Process 1: %d\n",pf_p1);
+	printf("Page Fault Process 2: %d\n",pf_p2);
+	printf("Page Fault Process 3: %d\n",pf_p3);
+
+	printf("\nPage Replacements:\n");
+	printf("Page Replacements Process 1: %d\n",pr_p1);
+	printf("Page Replacements Process 2: %d\n",pr_p2);
+	printf("Page Replacements Process 3: %d\n",pr_p3);
+	
 } 
 
-
+//THREAD FUNCTION TO RUN PROCESS 2
 void *p2_fun(void *vargp) 
 { 
-	int i,j;
-	int sum=0;
-	int current_page=14;
-	
-	while(current_page<=24){
-		if(main_memory_counter>=6)
-			main_memory_counter=0;
 
+	int i,j;
+	int current_page=14;
+	int replaced_page=-1;
+	
+	//PERFORM PAGING FOR NON-SHARED PAGES IN FCFS ORDER
+	while(current_page<=24){	
 		sem_wait(sem2);
-		//for(i=0;i<2 && current_page<25;i++){
-			//printf("\nPage Fault:%d",current_page);
-			MM[main_memory_counter++]=current_page;	
-			print_main_memory();			
-			sum=sum+do_sum(2,current_page);
-			current_page++;
-		//}
+		do_paging(&current_page,&replaced_page,&pf_p2,&pr_p2,&sum_p2,2);
+		current_page++;
 		sem_post(sem3);
-		sleep(2);
 	}
 
+	//MANAGING THE SHARED PAGES EXPLICITLY	
 	sem_wait(sem2);
-	MM[main_memory_counter++]=25;			
-	print_main_memory();
-	sum=sum+do_sum(2,25);
+	current_page=25;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p2,&pr_p2,&sum_p2,2);
 	sem_post(sem3);
-	sleep(2);
 
-	sem_wait(sem2);
-	MM[main_memory_counter++]=13;			
-	print_main_memory();
-	sum=sum+do_sum(2,13);
+	sem_wait(sem2);	
+	current_page=13;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p2,&pr_p2,&sum_p2,2);
 	sem_post(sem3);
-	sleep(2);
 
+	//PRINT THE FINAL SUM CALCULATED  BY PROCESS 2
+	printf("Sum by Process 2 :%d\n",sum_p2);
 } 
 
-
+//THREAD FUNCTION TO RUN PROCESS 3
 void *p3_fun(void *vargp) 
 { 
+	
 	int i,j;
-	int sum=0;
 	int current_page=26;
-	while(current_page<=36){
-		if(main_memory_counter>=6)
-			main_memory_counter=0;
+	int replaced_page=-1;
 
+	//PERFORM PAGING FOR NON-SHARED PAGES IN FCFS ORDER
+	while(current_page<=36){	
 		sem_wait(sem3);
-		//for(i=0;i<2 && current_page<=37;i++){
-			//printf("\nPage Fault:%d",current_page);
-			MM[main_memory_counter++]=current_page;	
-			print_main_memory();		
-			sum=sum+do_sum(3,current_page);
-			current_page++;
-		//}
+		do_paging(&current_page,&replaced_page,&pf_p3,&pr_p3,&sum_p3,3);
+		current_page++;
 		sem_post(sem1);
-		sleep(2);
 	}
 
+	//MANAGING THE SHARED PAGES EXPLICITLY	
 	sem_wait(sem3);
-	MM[main_memory_counter++]=37;			
-	print_main_memory();
-	sum=sum+do_sum(3,37);
+	current_page=37;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p3,&pr_p3,&sum_p3,3);
 	sem_post(sem1);
-	sleep(2);
 
-	sem_wait(sem3);
-	MM[main_memory_counter++]=25;			
-	print_main_memory();
-	sum=sum+do_sum(3,25);
+	sem_wait(sem3);	
+	current_page=25;
+	replaced_page=-1;
+	do_paging(&current_page,&replaced_page,&pf_p3,&pr_p3,&sum_p3,3);
 	sem_post(sem1);
-	sleep(2);
-	
+
+	//PRINT THE FINAL SUM CALCULATED  BY PROCESS 3
+	printf("Sum by Process 3 :%d\n",sum_p3);
 } 
 
 
-void dump_to_file(){
-	int i;
-	FILE *f;
-	f=fopen(input_file,"w");
-
-	for(i=1;i<=TOTAL_ELEMENTS;i++)	
-	fprintf(f,"%d\n",i);
-
-	fclose(f);
-}
-
+//TO READ 1470 ELEMENTS FROM FILE TO ARRAY
 void read_array(){
 
 	int sum=0;
-	int temp,i;
+	int temp,i,j;
 	FILE *f;
 	f=fopen(input_file,"r");
 
@@ -213,35 +288,62 @@ void read_array(){
 		sum += main_arr[i];
 	}
 
+	for(i=0;i<TOTAL_PAGES;i++){
+		if(i==36){
+			for(j=0;j<30;j++){
+				fscanf(f,"%d",&PAGE[i][j]);
+				printf("\nI %d : J %d",i,j);
+			}
+		}else{
+			for(j=0;j<PAGE_ENTRIES;j++){
+				fscanf(f,"%d",&PAGE[i][j]);
+				printf("\nI %d : J %d",i,j);
+			}
+		}
+	}
+
 	printf("Sum:%d\n",sum);
 	fclose(f);
 }
 
+//TO DUMP RANDOM VALUES TO FILE(RUN ONLY ONCE)
+void dump_to_file(){
 
+	int i;
+	FILE *f;
+	f=fopen(input_file,"w");
+	
+	//WRITING CONTENTS TO FILE
+	for(i=1;i<=TOTAL_ELEMENTS;i++)	
+		fprintf(f,"%d\n",i);
 
+	fclose(f);
+}
+
+//MAIN FUNCTION
 int main() 
 { 
 	int i; 
 	pthread_t tid1,tid2,tid3; 
-	int temp=123;
-	int *ptr=&temp;
 
+	//UNLINKING THE SEMAPHORES AND INITIALIZE FOR USE
 	sem_unlink("/sema1");	
 	sem_unlink("/sema2");
 	sem_unlink("/sema3");
+
 	sem1 = sem_open("/sema1", O_CREAT,  0644, 1);
 	sem2 = sem_open("/sema2", O_CREAT,  0644, 0);
 	sem3 = sem_open("/sema3", O_CREAT,  0644, 0);
 
-
+	dump_to_file();
 	read_array();
 	
-	pthread_create(&tid1,NULL, p1_fun, (void *)&temp); 
-	pthread_create(&tid2,NULL, p2_fun, (void *)&temp); 
+	//TO CREATE THREADS EACH FOR RESPECTIVE PROCESS
+	pthread_create(&tid1,NULL, p1_fun, (void *)&tid1); 
+	pthread_create(&tid2,NULL, p2_fun, (void *)&tid2); 
 	pthread_create(&tid3,NULL, p3_fun, (void *)&tid3); 
 
-	//initialize_PMT();
-
+	//WAITING FOR THREAD TO COMPLETE EXECUTION
 	pthread_join(tid1, NULL); 
 	pthread_join(tid2, NULL);
 	pthread_join(tid3, NULL);
--- thread_demo.c
+++ thread_demo.c
@@ -20,6 +20,7 @@ char input_file[] = "input_file.txt";
 
 int main_arr[TOTAL_ELEMENTS];
 int MM[6][2]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int PAGE[TOTAL_PAGES][PAGE_ENTRIES];
 
 int main_memory_counter=0;
 int sum_p1=0,sum_p2=0,sum_p3=0;
@@ -32,28 +33,31 @@ sem_t *sem1,*sem2,*sem3;
 void print_PCB(int process_state,int process_id,int program_counter,int page_no){
 
 	//CHECK FOR STATE AND PRINT ACCORDINGLY
+
+	printf("\n********** PCB Content **********\n");
 	if(process_state==0)
-		printf("State:Running\n");	
+		printf("State : Running\n");	
 	else if(process_state==1)
-		printf("State:Waiting\n");	
+		printf("State : Waiting\n");	
 	else
-		printf("State:Exit\n");	
-	printf("Process Id:%d\n",process_id);
-	printf("Program Counter:%d\n",program_counter);
-	printf("Memory Management Information:%d\n",page_no);
+		printf("State : Exit\n");	
+	printf("Process Id : %d\n",process_id);
+	printf("Program Counter : %d\n",program_counter);
+	printf("Memory Management Information : %d",page_no);
+	printf("\n*********************************\n");
 
 }
 
 //TO PRINT THE MAIN MEMORY CONTENT
 void print_main_memory(){
 
-	printf("Main Memory Content:\n");
-	printf("\n");
+	
+	printf("\nMain Memory Content:");
+	printf("\n************************************************************************\n");
 	for(int i=0;i<MAIN_MEMORY_SIZE;i++){		
 		printf("| P(%d):%d |",MM[i][1],MM[i][0]);
-
 	}
-	printf("\n");
+	printf("\n************************************************************************");
 	printf("\n");
 }
 
@@ -73,39 +77,43 @@ int do_sum(int process_id,int page_no){
 	page_no--;
 	int i;
 	int sum=0;
-	int start_index=page_no*40;
-	int end_index=(page_no*40)+PAGE_ENTRIES;
+	int start_index=0;
+	int end_index=PAGE_ENTRIES;
 
 	//CHECKING THE SHARED PAGES FOR CONDITIONS
 
 	//PAGE 25 IS SHARED BETWEEN PROCESS 2 AND 3
 	if(page_no == 24){
 		if(process_id==2){
-			start_index=page_no*40;
-			end_index=(page_no*40)+20;
+			start_index=0;
+			end_index=20;
 		}
 		if(process_id==3){
-			start_index=(page_no*40)+20;
-			end_index=(page_no*40)+PAGE_ENTRIES;
+			start_index=20;
+			end_index=PAGE_ENTRIES;
 		}
 	}
 
 	//PAGE 13 IS SHARED BETWEEN PROCESS 1 AND 3
 	if(page_no == 12){
 		if(process_id==1){
-			start_index=page_no*40;
-			end_index=(page_no*40)+10;			
+			start_index=0;
+			end_index=10;			
 		}
 		if(process_id==2){
-			start_index=(page_no*40)+10;
-			end_index=(page_no*40)+PAGE_ENTRIES;
+			start_index=10;
+			end_index=PAGE_ENTRIES;
 		}
+	}
+	if(page_no == 36){
+		start_index=0;
+		end_index=30;
 	}
 	
 	//CALCULATE THE SUM BASED ON CALCULATED INDICES
-	printf("No of Entries:%d\n",end_index-start_index);
+	printf("No of Array Entries Fetched:%d\n",end_index-start_index);
 	for(i=start_index;i<end_index;i++){
-		sum = sum + main_arr[i];
+		sum = sum + PAGE[page_no][i];
 	
 	}
 	
@@ -114,7 +122,8 @@ int do_sum(int process_id,int page_no){
 
 //TO PERFORM THE PAGE FAULT , REPLACEMENT AND PAGING STRATEGY
 void do_paging(int *current_page,int *replaced_page,int *pf_p,int *pr_p,int *sum_p,int process_id){
-
+	
+	printf("\n\n**********************************************************************************************************************");
 	*replaced_page=-1;
 
 	//RESET MAIN MEMORY COUNTER IF GREATER THAN MEMORY SIZE
@@ -122,32 +131,39 @@ void do_paging(int *current_page,int *replaced_page,int *pf_p,int *pr_p,int *sum
 		main_memory_counter=0;
 
 	//CHECK IF THE PAGE IS PRESENT IN MEMORY
-	if(!check_for_page(*current_page)){	
-
+	if(!check_for_page(*current_page)){			
+		
 		//DO NOT COUNT PAGE REPLACEMENT IF MM IS EMPTY (i.e. in the beginning)		
 		if(MM[main_memory_counter][0]!=-1){
 			*replaced_page=MM[main_memory_counter][0];
 			*pr_p=*pr_p+1;
 		}
+	
+		
 
 		*pf_p=*pf_p+1;
-		printf("\nPage Fault:%d\n",*current_page);
+		
+		printf("\nPAGE FAULT:%d\n",*current_page);
+		print_PCB(1,process_id,(*current_page - 1)*40,*current_page);
+		printf("\nPAGE %d IS READY\n",*current_page);
 
 		//IF PAGE IS REPLACED THEN PRINT INFORMATION
 		if(*replaced_page!=-1)
-			printf("Replacing Page: %d\n",*replaced_page);		
+			printf("\nREPLACING PAGE: %d\n",*replaced_page);	
+
+		print_PCB(0,process_id,(*current_page - 1)*40,*current_page);	
 
 		//UPDATE THE PAGE IN MAIN MEMORY	
 		MM[main_memory_counter][0]=*current_page;	
 		MM[main_memory_counter][1]=1;
 		main_memory_counter++;	
+		
+		print_main_memory();
 
 	//IF PAGE IS ALREADY IN MAIN MEMORY
 	}else{
-		printf("\nPage Hit:%d\n",*current_page);
-	}	
-	
-	print_main_memory();
+		printf("\nPAGE HIT:%d\n",*current_page);
+	}		
 
 	//ADD THE SUM OF CURRENT PAGE
 	*sum_p=*sum_p+do_sum(process_id,*current_page);
@@ -184,25 +200,30 @@ void *p1_fun(void *vargp)
 	sem_post(sem2);
 
 	//PRINT THE FINAL SUM CALCULATED
-	printf("Sum by Process 1 :%d\n",sum_p1);
-
+	sem_wait(sem1);
+	printf("Sum by Process 3 :%d\n",sum_p3);
 
 	//WAIT FOR BOTH THE PROCESS 2 AND 3 TO RETURN THE FINAL CONTENTS
-	sem_wait(sem1);
-	printf("\nSumming up partial sum of the processes...\n");
+
+	printf("\n\n**********************************************************************************************************************\n");
+	printf("GETTING DATA FROM PROCESS 2 AND 3 ...\n");
 	sum_p1=sum_p1+sum_p2+sum_p3;
-	printf("Final Sum : %d\n\n",sum_p1);	
+	printf("Final Sum : %d\n",sum_p1);	
 
 	printf("\nPage Faults:\n");
 	printf("Page Fault Process 1: %d\n",pf_p1);
 	printf("Page Fault Process 2: %d\n",pf_p2);
 	printf("Page Fault Process 3: %d\n",pf_p3);
+	printf("Total Page Faults: %d\n",pf_p1+pf_p2+pf_p3);
 
 	printf("\nPage Replacements:\n");
 	printf("Page Replacements Process 1: %d\n",pr_p1);
 	printf("Page Replacements Process 2: %d\n",pr_p2);
 	printf("Page Replacements Process 3: %d\n",pr_p3);
+	printf("Total Page Replacements: %d\n",pr_p1+pr_p2+pr_p3);
 	
+	print_PCB(2,1,(current_page-1)*40,current_page);	
+	sem_post(sem2);
 } 
 
 //THREAD FUNCTION TO RUN PROCESS 2
@@ -218,7 +239,7 @@ void *p2_fun(void *vargp)
 		sem_wait(sem2);
 		do_paging(&current_page,&replaced_page,&pf_p2,&pr_p2,&sum_p2,2);
 		current_page++;
-		sem_post(sem3);S
+		sem_post(sem3);
 	}
 
 	//MANAGING THE SHARED PAGES EXPLICITLY	
@@ -235,7 +256,10 @@ void *p2_fun(void *vargp)
 	sem_post(sem3);
 
 	//PRINT THE FINAL SUM CALCULATED  BY PROCESS 2
+	sem_wait(sem2);
 	printf("Sum by Process 2 :%d\n",sum_p2);
+	print_PCB(2,2,(current_page-1)*40,current_page);	
+	sem_post(sem3);
 } 
 
 //THREAD FUNCTION TO RUN PROCESS 3
@@ -268,7 +292,10 @@ void *p3_fun(void *vargp)
 	sem_post(sem1);
 
 	//PRINT THE FINAL SUM CALCULATED  BY PROCESS 3
+	sem_wait(sem3);
 	printf("Sum by Process 3 :%d\n",sum_p3);
+	print_PCB(2,3,(current_page-1)*40,current_page);	
+	sem_post(sem1);
 } 
 
 
@@ -276,18 +303,25 @@ void *p3_fun(void *vargp)
 void read_array(){
 
 	int sum=0;
-	int temp,i;
-	FILE *f;
+	int temp,i,j;
+	FILE *f;	
 	f=fopen(input_file,"r");
-
-	for(i=0;i<TOTAL_ELEMENTS;i++){
-		fscanf(f,"%d",&main_arr[i]);
-		//printf("%d\n",main_arr[i]);
-		sum += main_arr[i];
+	for(i=0;i<TOTAL_PAGES;i++){
+		if(i==36){
+			for(j=0;j<30;j++){
+				fscanf(f,"%d",&PAGE[i][j]);
+				sum+=PAGE[i][j];				
+			}
+		}else{
+			for(j=0;j<PAGE_ENTRIES;j++){
+				fscanf(f,"%d",&PAGE[i][j]);
+				sum+=PAGE[i][j];
+			}
+		}
 	}
-
-	printf("Sum:%d\n",sum);
 	fclose(f);
+	printf("Sum:%d\n",sum);
+	
 }
 
 //TO DUMP RANDOM VALUES TO FILE(RUN ONLY ONCE)
@@ -299,7 +333,7 @@ void dump_to_file(){
 	
 	//WRITING CONTENTS TO FILE
 	for(i=1;i<=TOTAL_ELEMENTS;i++)	
-		fprintf(f,"%d\n",1);
+		fprintf(f,"%d\n",i);
 
 	fclose(f);
 }
@@ -319,7 +353,7 @@ int main()
 	sem2 = sem_open("/sema2", O_CREAT,  0644, 0);
 	sem3 = sem_open("/sema3", O_CREAT,  0644, 0);
 
-	///dump_to_file();
+	dump_to_file();
 	read_array();
 	
 	//TO CREATE THREADS EACH FOR RESPECTIVE PROCESS
